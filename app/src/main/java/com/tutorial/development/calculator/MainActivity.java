3f12b5144d488e1a5d5fddc0ae0d2f07599faa37
lms <lms8147@gmail.com>
2018-08-11 21:46:08 +0900

    change ui{  * activity -> fragment }

--- app/src/main/java/com/tutorial/development/calculator/MainActivity.java
+++ app/src/main/java/com/tutorial/development/calculator/MainActivity.java
@@ -1,45 +1,28 @@
 package com.tutorial.development.calculator;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private EditText mEditTextOperandA;
-    private EditText mEditTextOperandB;
-    private TextView mTextViewResult;
-
-    private Calculator mCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCalculator = new Calculator();
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        mEditTextOperandA = findViewById(R.id.edit_text_operand_a);
-        mEditTextOperandB = findViewById(R.id.edit_text_operand_b);
-        mTextViewResult = findViewById(R.id.text_view_result);
-        Button button = findViewById(R.id.button_caculate);
+        if (fragment == null) {
+            fragment = MainFragment.newInstance();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int a = Integer.parseInt(mEditTextOperandA.getText().toString());
-                int b = Integer.parseInt(mEditTextOperandB.getText().toString());
 
-                int result = mCalculator.operateAdd(a,b);
-
-                mTextViewResult.setText("" + result);
-            }
-        });
     }
-
 
 }
---
+++ app/src/main/java/com/tutorial/development/calculator/MainFragment.java
@@ -0,0 +1,52 @@
+package com.tutorial.development.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    private EditText mEditTextOperandA;
+    private EditText mEditTextOperandB;
+    private TextView mTextViewResult;
+    private Calculator mCalculator;
+
+    public static MainFragment newInstance(){
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_main,container,false);
+
+        mCalculator = new Calculator();
+
+        mEditTextOperandA = root.findViewById(R.id.edit_text_operand_a);
+        mEditTextOperandB = root.findViewById(R.id.edit_text_operand_b);
+        mTextViewResult = root.findViewById(R.id.text_view_result);
+        Button button = root.findViewById(R.id.button_caculate);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(mEditTextOperandA.getText().toString());
+                int b = Integer.parseInt(mEditTextOperandB.getText().toString());
+
+                int result = mCalculator.operateAdd(a,b);
+
+                mTextViewResult.setText("" + result);
+            }
+        });
+        return root;
+    }
+
+}
